a7ab6d4c3090f912155356ad74161e7a9c490a77
gunja24bothra <gunjan.bothra@driscolls.com>
2020-12-10 19:19:04 +0530

    test

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+const WidgetDivs = document.querySelectorAll('.reddit_widget');
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+WidgetDivs.forEach(Div => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App domElement={Div}  />
+    </React.StrictMode>,
+    Div
+  );
+})
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
